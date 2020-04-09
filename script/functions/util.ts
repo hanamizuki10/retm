7b3a7bf96d12faec81dd37b7a6ecc0374096fb95
hanamizuki10 <mizuki.hana@imprich-create.site>
2020-04-09 14:46:39 +0900

    サンプルとして追加

--- script/functions/util.ts
+++ script/functions/util.ts
@@ -4,3 +4,8 @@ export const getDayFormat = (date = new Date()): string => {
   const day = date.getDate();
   return `${year}-${month}-${day}`;
 };
+export const toDate = (str: string, separator: string): Date => {
+  var arr = str.split(separator);
+  var realDigits = arr.map(Number)
+  return new Date(realDigits[0], realDigits[1] - 1, realDigits[2]);
+};
