cebe4f352882da742d9ff661ba382f3a76b6ad0b
hanamizuki10 <mizuki.hana@imprich-create.site>
2020-04-09 14:47:35 +0900

    カレンダーの１セルのコンポーネントを指定データ型を変更しそれに伴う修正

--- src/components/CalendarCell.vue
+++ src/components/CalendarCell.vue
@@ -1,18 +1,26 @@
 <template>
-  <table>
+  <table :class="cellColor">
     <tr>
       <td>{{ data.date.getDate() }}</td>
       <td></td>
-      <td>{{ data.text }}</td>
-      <td>{{ data.holidayName }}</td>
+      <td colspan="2">{{ data.holidayName }}</td>
     </tr>
     <tr>
-      <td colspan="4" class="text-align-center">{{ data.planTime.inputTime }}</td>
+      <td colspan="4" class="text-align-center planTime">
+        <v-text-field name="planTime" v-model="data.planTime.inputTime" outlined></v-text-field>
+      </td>
     </tr>
     <tr v-for="(categoryName, index) in categoryNames" :key="categoryName">
       <td></td>
-      <td>{{ categoryName }}</td>
-      <td>{{ data.categoryTimes[index].inputTime }}</td>
+      <td colspan="2" class="text-align-center categoryTimes">
+        <v-text-field
+          name="categoryTimes"
+          :label="categoryName"
+          v-model="data.categoryTimes[index].inputTime"
+          outlined
+          dense
+        ></v-text-field>
+      </td>
       <td></td>
     </tr>
     <tr>
@@ -22,6 +30,11 @@
     <tr>
       <td colspan="3" class="text-align-right">残時間</td>
       <td>{{ data.remainingTime.inputTime }}</td>
+    </tr>
+    <tr>
+      <td colspan="4" class="text-align-center text">
+        <v-textarea name="text" label="備考" v-model="data.text" rows="1" outlined></v-textarea>
+      </td>
     </tr>
   </table>
 </template>
@@ -34,6 +47,16 @@ export default class CalendarCell extends Vue {
   //@Prop() private data!: CustomTypes.MyDay;
   @Prop() private data!: any;
   @Prop() private categoryNames!: String[];
+
+  get cellColor() {
+    if ('土' == this.data.week) {
+      return 'saturday';
+    } else if ('日' == this.data.week) {
+      return 'sunday';
+    } else {
+      return '';
+    }
+  }
 }
 </script>
 
@@ -46,5 +69,23 @@ export default class CalendarCell extends Vue {
 }
 table.calendar td {
   border: none;
+}
+.saturday {
+  background-color: aqua;
+}
+.sunday {
+  background-color: burlywood;
+}
+.planTime div {
+  width: 100px;
+}
+.planTime input {
+  text-align: center;
+}
+.categoryTimes div {
+  width: 100px;
+}
+.text div {
+  width: 100px;
 }
 </style>
--- tests/unit/CalendarCell.spec.ts
+++ tests/unit/CalendarCell.spec.ts
@@ -3,14 +3,30 @@ import CalendarCell from '@/components/CalendarCell.vue';
 
 describe('CalendarCell.vue', () => {
   it('renders props.msg when passed', () => {
-    const day = '1';
-    const holidayName = '日本の祝日';
-    const wrapper = shallowMount(CalendarCell, {
-      propsData: { day, holidayName }
-    });
-    expect(wrapper.props().day).toBe(day);
-    expect(wrapper.props().holidayName).toBe(holidayName);
-    expect(wrapper.contains('table')).toBe(true);
 
+    const data: CustomTypes.MyDay = {
+      date: new Date(2020, 0, 1),
+      isTarget: false,
+      isCurrent: false,
+      isHoliday: false, // TODO
+      holidayName: '', // TODO
+      week: '土',
+      planTime: { inputTime: '', hours: 0, minutes: 0 },
+      totalTime: { inputTime: '', hours: 0, minutes: 0 },
+      remainingTime: { inputTime: '', hours: 0, minutes: 0 },
+      categoryTimes: [
+        { inputTime: '', hours: 0, minutes: 0 },
+        { inputTime: '', hours: 0, minutes: 0 },
+        { inputTime: '', hours: 0, minutes: 0 }
+      ],
+      text: ''
+    };
+    const categoryNames = ['A', 'M', '他'];
+    const wrapper = shallowMount(CalendarCell, {
+      propsData: { data, categoryNames }
+    });
+    expect(wrapper.props().data).toBe(data);
+    expect(wrapper.props().categoryNames).toBe(categoryNames);
+    expect(wrapper.contains('table')).toBe(true);
   });
 });
