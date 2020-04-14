057cd7d392f9dc268bfeb41bc4416ffc8b5bceab
hanamizuki10 <mizuki.hana@imprich-create.site>
2020-04-14 14:12:49 +0900

    vuexを使ってデータをやり取りするよう叩き台を作成、合わせてレイアウトを少し見直し

--- src/google/script.ts
+++ src/google/script.ts
@@ -3,7 +3,12 @@ export class GoogleScript {
     return this.run('getSampleString');
   }
   public getHolidays(startDate: string, endDate: string): Promise<GetHolidayResult> {
-    return this.run('getHolidays', startDate, endDate);
+    console.log(process.env.NODE_ENV);
+    if (process.env.NODE_ENV === 'production') {
+      return this.run('getHolidays', startDate, endDate);
+    } else {
+      return Promise.resolve({ '2020-03-20': '春分の日', '2020-04-29': '昭和の日' });
+    }
   }
 
   private run(functionName: string, ...args: any[]): Promise<any> {
--- src/types/script.d.ts
+++ src/types/script.d.ts
@@ -2,26 +2,36 @@ declare interface GetHolidayResult {
   [key: string]: string;
 }
 declare namespace CustomTypes {
+  export interface HolidayInfo {
+    keyDayString: string;
+    holidayName: string;
+  }
+
+  export interface MyDays {
+    [key: string]: MyDay;
+  }
+
   export interface MyMonth {
     firstDate: Date | null;
     lastDate: Date | null;
     weeks: MyWeek[];
   }
   export interface MyWeek {
-    days: MyDay[];
+    days: string[];
   }
   export interface MyDay {
     date: Date;
-    isTarget: Boolean;
-    isCurrent: Boolean;
-    isHoliday: Boolean;
-    holidayName: String;
-    week: String;
+    keyDayString: string;
+    isTarget: boolean;
+    isCurrent: boolean;
+    isHoliday: boolean;
+    holidayName: string;
+    week: string;
     planTime: MyTime;
     totalTime: MyTime;
     remainingTime: MyTime;
     categoryTimes: MyTime[];
-    text: String;
+    text: string;
   }
   export interface MyTime {
     hours: number;
