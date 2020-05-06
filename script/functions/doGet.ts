10b714a45aa5db079d4698a66da3bbe403e2ac81
hanamizuki10 <mizuki.hana@imprich-create.site>
2020-05-06 17:09:30 +0900

    タイトル変更

--- script/functions/doGet.ts
+++ script/functions/doGet.ts
@@ -1,5 +1,5 @@
 export function doGet(): GoogleAppsScript.HTML.HtmlOutput {
   const htmlOutput = HtmlService.createTemplateFromFile('index').evaluate();
-  htmlOutput.setTitle('GAS+Vue.js').addMetaTag('viewport', 'width=device-width, initial-scale=1');
+  htmlOutput.setTitle('残業時間管理くん').addMetaTag('viewport', 'width=device-width, initial-scale=1');
   return htmlOutput;
 }
