d624ecf69ce928e34d13cc437b41957e0bd60b62
hanamizuki10 <mizuki.hana@imprich-create.site>
2020-05-18 11:42:57 +0900

    カテゴリ別時間情報のみを表示するようにレイアウトを見直し

--- src/components/ResultTable.vue
+++ src/components/ResultTable.vue
@@ -1,16 +1,19 @@
 <template>
-  <v-data-table
-    dense
-    :headers="headers"
-    :items="times"
-    item-key="name"
-    class="elevation-1"
-    hide-default-footer
-  >
-    <template v-slot:item.remainingTime="{ item }">
-      <span :class="getFontColor(item.remainingTime)">{{ item.remainingTime }}</span>
-    </template>
-  </v-data-table>
+  <div>
+    <div class="subtitle-2 font-weight-bold text-align-left">カテゴリ別時間情報</div>
+    <v-data-table
+      dense
+      :headers="headers"
+      :items="times"
+      item-key="name"
+      class="elevation-1 width-360"
+      hide-default-footer
+    >
+      <template v-slot:item.remainingTime="{ item }">
+        <span :class="getFontColor(item.remainingTime)">{{ item.remainingTime }}</span>
+      </template>
+    </v-data-table>
+  </div>
 </template>
 <script lang="ts">
 import { Component, Prop, Vue, Emit } from 'vue-property-decorator';
@@ -18,26 +21,11 @@ import calendardata from '../store/modules/data';
 
 @Component
 export default class ResultTable extends Vue {
-  get scheduledTime(): CustomTypes.MyTime {
-    return calendardata.moduleInputTimes.scheduledTime;
-  }
-  get actualTime(): CustomTypes.MyTime {
-    return calendardata.moduleInputTimes.actualTime;
-  }
-  get remainingTime(): CustomTypes.MyTime {
-    return calendardata.moduleInputTimes.remainingTime;
-  }
   get categories(): CustomTypes.Category[] {
     return calendardata.moduleCategories;
   }
   get times(): any {
     var data = [];
-    data.push({
-      name: '総',
-      scheduledTime: this.scheduledTime.strHours + ':' + this.scheduledTime.strMinutes,
-      actualTime: this.actualTime.strHours + ':' + this.actualTime.strMinutes,
-      remainingTime: this.remainingTime.strHours + ':' + this.remainingTime.strMinutes
-    });
     this.categories.forEach((category: CustomTypes.Category) => {
       data.push({
         name: category.name,
@@ -57,9 +45,9 @@ export default class ResultTable extends Vue {
         sortable: false,
         value: 'name'
       },
-      { text: '予定時間', align: 'end', width: 100, sortable: false, value: 'scheduledTime' },
+      { text: '予定時間', align: 'end', width: 80, sortable: false, value: 'scheduledTime' },
       { text: '累積総時間', align: 'end', width: 100, sortable: false, value: 'actualTime' },
-      { text: '残時間', align: 'end', width: 100, sortable: false, value: 'remainingTime' }
+      { text: '残時間', align: 'end', width: 80, sortable: false, value: 'remainingTime' }
     ];
   }
   private getFontColor(value: string) {
@@ -70,3 +58,12 @@ export default class ResultTable extends Vue {
   }
 }
 </script>
+
+<style scoped>
+.text-align-left {
+  text-align: left;
+}
+.width-360 {
+  width: 360px;
+}
+</style>
